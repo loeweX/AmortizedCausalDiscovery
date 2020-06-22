23a1cfb7dd2634ebb9e2f8f0bd5f0cc20ca0b131
Sindy Löwe <loeweX@users.noreply.github.com>
2020-06-22 16:48:25 +0200

    Update setup_dependencies.sh

--- setup_dependencies.sh
+++ setup_dependencies.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 echo "Make sure conda is installed."
 echo "Installing environment:"
-conda env create -f environment.yml || conda env update -f environment.yml || exit
-conda activate causalinf
+conda env create -f environment.yml
+conda activate ACD
 cd codebase
